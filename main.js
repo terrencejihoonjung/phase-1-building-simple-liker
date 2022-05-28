b20448f546f6e57287675c6e81a7c3dae4ec3b1b
--replace-all <terrencejung928@gmail.com>
2022-05-27 18:54:41 -0700

    commit tree 96e5635004acfb417ec7c463b07f220b528c35de

--- main.js
+++ main.js
@@ -2,10 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Grab the empty heart element
+const hearts = document.querySelectorAll(".like");
 
+//Iterate through each heart icon
+hearts.forEach(likeHeart)
 
-
+//For each heart, listen for a click event. If clicked, fill the heart. 
+function likeHeart(heart) {
+  heart.addEventListener("click", e => {
+    mimicServerCall()
+    .then(data => {
+      const glyph = heart.querySelector("span");
+      if (glyph.textContent === EMPTY_HEART) {
+        glyph.textContent = FULL_HEART;
+        glyph.classList.add("activated-heart");
+      }
+      else {
+        glyph.textContent = EMPTY_HEART;
+        glyph.classList.remove("activated-heart");
+      }
+    })
+    .catch(error => {
+      //Display error modal and server error message
+      const errorMessage = document.querySelector("div#modal");
+      errorMessage.classList.remove("hidden");
+      document.querySelector("#modal-message").textContent = error;
+      setTimeout(() => errorMessage.classList.add("hidden"), 3000);
+    })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
